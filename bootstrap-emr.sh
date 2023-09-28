b07bcabeabe323a22d71a4bfff5a9fe1869bb569
xavier.parisot@gmail.com <xavier.parisot@gmail.com>
2023-09-28 18:48:25 +0200

    Ajout Boostrap

---
+++ bootstrap-emr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo python3 -m pip install -U setuptools
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install wheel
+sudo python3 -m pip install pillow
+sudo python3 -m pip install pandas
+sudo python3 -m pip install pyarrow
+sudo python3 -m pip install boto3
+sudo python3 -m pip install s3fs
+sudo python3 -m pip install fsspec
+sudo python3 -m pip install matplotlib
+sudo python3 -m pip install numpy
+sudo python3 -m pip install tensorflow
+sudo python3 -m pip install ipywidgets
